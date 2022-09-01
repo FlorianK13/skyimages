fca7a405d7e70c84dc526f12161f7f908a731b26
Florian Kotthoff <florian.kotthoff@posteo.de>
2022-09-01 16:34:51 +0200

    Extend package sceleton

--- skyimages/download/download.py
+++ skyimages/download/download.py
@@ -1,9 +1,18 @@
+from skyimages.utils.path import file_exists
+
 def DownloadClass():
-    def __init__():
+    def __init__(self):
+        pass
+    
+    def _download_from_zenodo(url_dict:dict):
         pass
 
 
 def FolcomDownloader(DownloadClass):
-    def __init__():
-        pass
+    def __init__(self, root_dir):
+        self.root_dir = root_dir
+        self.url_dict = {}
+        if not file_exists(self.root_dir):
+            self._download_from_zenodo(self.url_dict)
+            
     
---
+++ skyimages/utils/path.py
@@ -0,0 +1,13 @@
+def file_exists(root_dir: str) -> bool:
+    """Checks if the file exists
+
+    Parameters
+    ----------
+    root_dir : str
+        Relative or absolute path
+
+    Returns
+    -------
+    bool
+        True if the folder exists
+    """
