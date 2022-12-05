24f372f1e31d4c268b34bd2a97f2822ec78092e0
Florian Kotthoff <florian.kotthoff@posteo.de>
2022-12-05 16:07:31 +0100

    Update file size

--- skyimages/constants.py
+++ skyimages/constants.py
@@ -51,7 +51,7 @@ SKIPPD_DOWNLOAD_URLS = {
     "dataset": {
         "filename": "2017_2019_images_pv_processed.hdf5",
         "url": "https://stacks.stanford.edu/file/druid:dj417rh1007/2017_2019_images_pv_processed.hdf5",
-        "size[MB]": 4557,
-        "size[Bit]": 38226886656,
+        "size[MB]": 4262,
+        "size[Bit]": int(4262 * 1024 * 1024 * 8),
     },
 }
