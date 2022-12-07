c9f84e8d575de6135cefc47edc0bc78d3e35c075
Florian Kotthoff <florian.kotthoff@posteo.de>
2022-12-07 13:42:36 +0100

    Add dataloader test

--- tests/test_dataset_skippd.py
+++ tests/test_dataset_skippd.py
@@ -1,5 +1,7 @@
 from skyimages.dataset import SKIPPDDataSet
 import pytest
+import torchvision.transforms as transforms
+import torch
 import pdb
 
 
@@ -7,17 +9,20 @@ import pdb
 def skippd_testset():
     return SKIPPDDataSet(download=False, train=False)
 
+@pytest.fixture
+def skippd_transformed_testset():
+    transform = transforms.ToTensor()
+    return SKIPPDDataSet(download=False, train=False, transform=transform)
 
-# @pytest.fixture
-# def skippd_trainset():
-#    return SKIPPDDataSet(download=False, train=True)
 
 
 def test_skippd_testset_dataset(skippd_testset):
 
-    # data inof
+    # data ino
     assert len(skippd_testset.data_info) == 4
     assert skippd_testset.data_info[0]["shape"] == (14003, 64, 64, 3)
+    
+    # data length
     assert len(skippd_testset) == 14003
 
     dataset_path = skippd_testset.data_info[0]["file_path"]
@@ -25,3 +30,16 @@ def test_skippd_testset_dataset(skippd_testset):
     # data cache
     assert len(skippd_testset.data_cache) == 1
     assert len(skippd_testset.data_cache[dataset_path])
+    
+    # data get item
+    assert skippd_testset[0][0].shape==torch.Size([64, 64, 3])
+    
+
+def test_skippd_testset_dataloader(skippd_transformed_testset):
+    batch_size = 5
+    dataloader = torch.utils.data.DataLoader(skippd_transformed_testset, batch_size=batch_size, shuffle=False, num_workers=0)
+    batch = next(iter(dataloader))
+    
+    assert len(batch) == 2
+    assert len(batch[0]) == batch_size
+    
